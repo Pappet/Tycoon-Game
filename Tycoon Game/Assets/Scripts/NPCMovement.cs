6e6c713a05d78a937bfde82a4920221b802bf68e
Pappet <pappet@web.de>
2017-08-14 12:33:24 +0200

    Basic NavMeshMovement

---
+++ Tycoon Game/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMovement : MonoBehaviour {
+
+    NavMeshAgent navMeshAgent;
+
+    public List<Transform> Waypoints = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Waypoints.Count > 0)
+        {
+            navMeshAgent.destination = Waypoints[0].position;
+        }
+
+        if (Vector3.Distance(transform.position, Waypoints[0].position) < 1.5f && Waypoints.Count >= 0)
+        {
+            Waypoints.Remove(Waypoints[0]);
+        }
+	}
+}
