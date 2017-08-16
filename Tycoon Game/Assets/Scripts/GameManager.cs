7db2904046feb722401b1d818ae3172f0d802614
Pappet <pappet@web.de>
2017-08-16 20:29:23 +0200

    Visitor simple AI
    Simple AI for Visitors

---
+++ Tycoon Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform Spawnpoint;
+    public GameObject Visitor;
+    public Text VisitorCountText;
+    int VisitorCount = 0;
+    List<GameObject> Visitors = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        VisitorCountText.text = VisitorCount.ToString();
+    }
+
+    public void AddVisitor()
+    {
+        GameObject g = Instantiate(Visitor, Spawnpoint.position, Quaternion.identity);
+        Visitors.Add(g);
+        VisitorCount++;        
+    }
+
+    public void RemoveVisitor(GameObject g)
+    {
+        VisitorCount--;
+        Visitors.Remove(g);
+        Destroy(g);
+    }
+}
--- Tycoon Game/Assets/Scripts/NPCMovement.cs
+++ Tycoon Game/Assets/Scripts/NPCMovement.cs
@@ -6,24 +6,83 @@ using UnityEngine.AI;
 public class NPCMovement : MonoBehaviour {
 
     NavMeshAgent navMeshAgent;
+    float Timer = 0f;
+    int VisitedObjects = 0;
+    int ObjectsToVisit = 0;
+    float DistanceToObject = 0f;
+    float WaitTime = 2f;
 
-    public List<Transform> Waypoints = new List<Transform>();
+    List<Transform> Waypoints = new List<Transform>();
+    public Transform Exit;
+    public GameManager gameManager;
+    public bool ReachedExit = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Waypoints.Count > 0)
+
+        if (gameManager == null)
         {
-            navMeshAgent.destination = Waypoints[0].position;
+            gameManager = FindObjectOfType<GameManager>();
         }
 
-        if (Vector3.Distance(transform.position, Waypoints[0].position) < 1.5f && Waypoints.Count >= 0)
+        if (Exit == null)
         {
-            Waypoints.Remove(Waypoints[0]);
+            Exit = GameObject.FindGameObjectWithTag("Exit").GetComponent<Transform>();
         }
-	}
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("DisplayItem"))
+        {
+            Waypoints.Add(g.transform);
+            ObjectsToVisit++;
+        }
+
+        Utilitys.ShuffleArray<Transform>(Waypoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VisitedObjects < ObjectsToVisit)
+        {
+            DistanceToObject = Vector3.Distance(Waypoints[VisitedObjects].position, transform.position);
+        }
+
+        if (ObjectsToVisit > VisitedObjects && DistanceToObject > 2f)
+        {            
+            MoveToWaypoint(Waypoints[VisitedObjects]);
+        }
+
+        if (DistanceToObject < 2f && Timer <= WaitTime)
+        {
+            //navMeshAgent.enabled = false;
+            Timer += Time.deltaTime;
+        }
+
+        if (Timer >= WaitTime && ObjectsToVisit > VisitedObjects)
+        {
+            //navMeshAgent.enabled = true;
+            VisitedObjects++;
+            Timer = 0;
+        }
+
+        if (VisitedObjects == ObjectsToVisit)
+        {
+            //navMeshAgent.enabled = true;
+            MoveToWaypoint(Exit);
+        }
+
+        if (Vector3.Distance(Exit.position, transform.position) <= 2f && VisitedObjects == ObjectsToVisit)
+        {
+            ReachedExit = true;
+            gameManager.RemoveVisitor(this.gameObject);
+        }
+    }
+    
+    void MoveToWaypoint(Transform way)
+    {
+        navMeshAgent.destination = way.position;
+    }
+
 }
